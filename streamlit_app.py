e4f1b0576c9e8036aef3c4e40f335e49e92369e2
cenbek53 <62766705+cenbek53@users.noreply.github.com>
2024-08-06 15:45:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("CRM MOBILE SALES")
 df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
