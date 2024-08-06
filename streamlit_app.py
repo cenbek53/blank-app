619afa7340f7fef1c8ed71ad2224dff0db44ba1b
cenbek53 <62766705+cenbek53@users.noreply.github.com>
2024-08-06 16:03:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 def home():
     st.write("sss")
 
 def customer():
-    st.write("ss")
+    st.title("Customer")    
+    df = pd.DataFrame(
+        np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
+    st.table(df)
 
 def contacts():
     st.write("ss")
