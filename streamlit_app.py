df072d079cdaaf08eba971aa6b414bbe02d5765d
cenbek53 <62766705+cenbek53@users.noreply.github.com>
2024-08-06 15:52:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 
 def page1():
-    st.write(st.session_state.foo)
+    st.write("sss")
 
 def page2():
-    st.write(st.session_state.bar)
-
-# Widgets shared by all the pages
-st.sidebar.selectbox("Foo", ["A", "B", "C"], key="foo")
-st.sidebar.checkbox("Bar", key="bar")
+    st.write("ss)
 
 pg = st.navigation([st.Page(page1), st.Page(page2)])
 pg.run()
