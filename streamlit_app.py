9283a23132905bdd03bff798f60bbe5b5dff9475
cenbek53 <62766705+cenbek53@users.noreply.github.com>
2024-08-06 15:43:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "test"
-)
+st.title("CRM MOBILE SALES")
+st.dataframe(data=None, width=None, height=None, *, use_container_width=False, hide_index=None, column_order=None, column_config=None, key=None, on_select="ignore", selection_mode="multi-row")
